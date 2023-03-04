53e5e1ba6bf6447f9808511f6f9bce497b75432a
AlexanderMatiushchenko <125497841+AlexanderMatiushchenko@users.noreply.github.com>
2023-03-04 15:18:11 +0100

    Add files via upload

---
+++ New 1/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = 8;
+        String opponent = "Who play: Bavaria vs PSG";
+        boolean winBavaria = true;
+        double number1 = 300;
+        double number2 = 156;
+        double result = number1 + number2;
+        System.out.println(number);
+        System.out.println(opponent);
+        System.out.println(winBavaria);
+        System.out.println(result);
+    }
+}
